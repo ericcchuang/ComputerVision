e356d3c361c2ec701acdd3054bd404800d82a40a
CISSEYH <ehuang@njtransit.com>
2025-08-13 11:10:15 -0400

    Revert "debug"
    This reverts commit ac4d6b811cc26e5c33baf41379b1f87a7c917385.

--- ui/app/_layout.tsx
+++ ui/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f5843d",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontFamily: "Arial",
+        },
+        headerTitleAlign: "center",
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
 }
