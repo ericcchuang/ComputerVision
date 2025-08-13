3d0b02cd7878d7aa8c54752198e667081437bbf1
CISSEYH <ehuang@njtransit.com>
2025-08-13 09:18:58 -0400

    slop that i know doesn't work

--- ui/app/_layout.tsx
+++ ui/app/_layout.tsx
@@ -1,17 +1,21 @@
 import { Stack, Slot } from "expo-router";
 
 export default function RootLayout() {
-  return < Slot />; {/*<Stack
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "#f5843d",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        fontWeight: "bold",
-        fontFamily: "Arial",
-      },
-      headerTitleAlign: "center",
-    }} >
-  </Stack>;*/}
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f5843d",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+          fontFamily: "Arial",
+        },
+        headerTitleAlign: "center",
+      }}
+    >
+      <Slot />
+    </Stack>
+  );
 }
