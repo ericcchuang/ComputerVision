1659b337f31369f4c9168e1efe3beae2dc24e5db
CISSEYH <ehuang@njtransit.com>
2025-08-13 10:23:19 -0400

    router basename

--- ui/app/_layout.tsx
+++ ui/app/_layout.tsx
@@ -1,10 +1,6 @@
 import { Stack } from "expo-router";
 import Constants from "expo-constants";
 
-export const unstable_settings = {
-  basePath: Constants.expoConfig?.extra?.routerBasePath || "/",
-};
-
 export default function RootLayout() {
   return (
     <Stack
