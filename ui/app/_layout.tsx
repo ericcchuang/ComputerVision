2724ebbd5bdaa0cae62b44a252cd821717c0dc5b
CISSEYH <ehuang@njtransit.com>
2025-08-14 10:32:44 -0400

    even more router slop

--- ui/app/_layout.tsx
+++ ui/app/_layout.tsx
@@ -1,22 +1,25 @@
 import { Stack, Slot } from "expo-router";
+import { BrowserRouter } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f5843d",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-          fontFamily: "Arial",
-        },
-        headerTitleAlign: "center",
-      }}
-      initialRouteName="index"
-    >
-      <Slot />
-    </Stack>
+    <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f5843d",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontFamily: "Arial",
+          },
+          headerTitleAlign: "center",
+        }}
+        initialRouteName="index"
+      >
+        <Slot />
+      </Stack>
+    </BrowserRouter>
   );
 }
