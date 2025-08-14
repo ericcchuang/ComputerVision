51cf02bf3db62061b92d42b94de9d32d7c34fc90
CISSEYH <ehuang@njtransit.com>
2025-08-14 09:01:05 -0400

    app config js

---
+++ ui/app.config.js
@@ -0,0 +1,14 @@
+export default {
+    expo: {
+        web: {
+            favicon: "./assets/favicon.png",
+            publicPath: "/ComputerVision/",
+            output: "static"
+        },
+        extra: {
+            router: {
+                basePath: "/ComputerVision"
+            }
+        }
+    }
+};
